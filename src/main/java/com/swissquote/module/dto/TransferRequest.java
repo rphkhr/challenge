76e5891bc84e69f74e9976f75bb08e7350af8f88
Raphael <raphael.khoury@foo-solutions.com>
2018-11-03 15:27:08 +0200

    Implemented cashout, transfer todo: cleanup, comment, currency check

--- src/main/java/com/swissquote/module/dto/TransferRequest.java
+++ src/main/java/com/swissquote/module/dto/TransferRequest.java
@@ -1,5 +1,9 @@
 package com.swissquote.module.dto;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
 import java.math.BigDecimal;
 
 public class TransferRequest {
@@ -8,4 +12,72 @@ public class TransferRequest {
     private String destinationAccountId;
     private BigDecimal amount;
     private String currency;
+
+    public String getSourceAccountId() {
+        return sourceAccountId;
+    }
+
+    public void setSourceAccountId(String sourceAccountId) {
+        this.sourceAccountId = sourceAccountId;
+    }
+
+    public String getDestinationAccountId() {
+        return destinationAccountId;
+    }
+
+    public void setDestinationAccountId(String destinationAccountId) {
+        this.destinationAccountId = destinationAccountId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (!(o instanceof TransferRequest)) return false;
+
+        TransferRequest that = (TransferRequest) o;
+
+        return new EqualsBuilder()
+                .append(sourceAccountId, that.sourceAccountId)
+                .append(destinationAccountId, that.destinationAccountId)
+                .append(amount, that.amount)
+                .append(currency, that.currency)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(sourceAccountId)
+                .append(destinationAccountId)
+                .append(amount)
+                .append(currency)
+                .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("sourceAccountId", sourceAccountId)
+                .append("destinationAccountId", destinationAccountId)
+                .append("amount", amount)
+                .append("currency", currency)
+                .toString();
+    }
 }
--- src/main/java/com/swissquote/module/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/swissquote/module/service/impl/TransactionServiceImpl.java
@@ -42,11 +42,54 @@ public class TransactionServiceImpl implements TransactionService {
 
     @Override
     public Transaction cashOut(CashInOutRequest cashInOutRequest) {
-        return null;
+        Account account = accountRepository.findOne(cashInOutRequest.getAccountId());
+        if (account == null) return null;
+
+        if (account.getBalance().subtract(cashInOutRequest.getAmount()).compareTo(BigDecimal.ZERO) < 1) {
+            return null;
+        }
+
+        Transaction transaction = new Transaction();
+        transaction.setAmount(cashInOutRequest.getAmount());
+        transaction.setCurrency(cashInOutRequest.getCurrency());
+        transaction.setSourceAccount(account);
+        transaction.setTransactionType(TransactionType.CASHOUT.name());
+        BigDecimal newAccountBalance = account.getBalance().subtract(cashInOutRequest.getAmount());
+
+        accountRepository.setNewAccountBalance(account.getId(), newAccountBalance);
+
+        return transactionRepository.save(transaction);
     }
 
     @Override
-    public Transaction transfer(TransferRequest transferRequest) {
-        return null;
+    public Transaction transfer(TransferRequest request) {
+        Account srcAccount = accountRepository.findOne(request.getSourceAccountId());
+        if (srcAccount == null) return null;
+
+        Account dstAccount = accountRepository.findOne(request.getDestinationAccountId());
+        if (dstAccount == null) return null;
+
+        if (srcAccount.getBalance().subtract(request.getAmount()).compareTo(BigDecimal.ZERO) < 1) {
+            return null;
+        }
+
+        //Create transaction object
+        Transaction transaction = new Transaction();
+        transaction.setAmount(request.getAmount());
+        transaction.setCurrency(request.getCurrency());
+        transaction.setSourceAccount(srcAccount);
+        transaction.setTargetAccount(dstAccount);
+        transaction.setTransactionType(TransactionType.TRANSFER.name());
+
+        //Calculate new amounts & update
+        BigDecimal newSourceAccountBalance = srcAccount.getBalance().subtract(request.getAmount());
+        BigDecimal newDestinationAccountBalance = dstAccount.getBalance().add(request.getAmount());
+
+        accountRepository.setNewAccountBalance(srcAccount.getId(), newSourceAccountBalance);
+
+        accountRepository.setNewAccountBalance(dstAccount.getId(), newDestinationAccountBalance);
+
+        // return saved transaction
+        return transactionRepository.save(transaction);
     }
 }
